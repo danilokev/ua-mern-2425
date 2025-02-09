dadf31ea25a7b531ca3e1c8c272df209ed954b73
kdao <kdao2@alu.ua.es>
2025-02-09 16:42:50 +0100

    Actualizado goalController.js

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,37 +1,39 @@
 'use strict'
+const asyncHandler = require('express-async-handler');
+
 // @desc Get goals
 // @route GET /api/goals
 // @access Private
-const getGoals = (req, res) => {
+const getGoals = asyncHandler(async (req, res) => {
   res.status(200).json({ message: 'Get goals' });
-}
+});
 
 // @desc Set goal
 // @route POST /api/goals
 // @access Private
-const setGoal = (req, res) => {
+const setGoal = asyncHandler(async (req, res) => {
   if (!req.body.text) {
     res.status(400);
     throw new Error('Introduce un texto válido');
   }
   res.status(200).json({ message: 'Set goal' });
-}
+});
 
 // @desc Update goal
 // @route Update /api/goals/:id
 // @access Private
-const updateGoal = (req, res) => {
+const updateGoal = asyncHandler(async (req, res) => {
   const id = req.params.id;
   res.status(200).json({ message: `Update goal ${id}` });
-}
+});
 
 // @desc Delete goal
 // @route DELETE /api/goals/:id
 // @access Private
-const deleteGoal = (req, res) => {
+const deleteGoal = asyncHandler(async (req, res) => {
   const id = req.params.id;
   res.status(200).json({ message: `Delete goal ${id}` });
-}
+});
 
 module.exports = {
   getGoals,
