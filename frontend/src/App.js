980aa2861a1ef7526191cefdd366af972896a3c1
Kevin Danilo <kdao2@alu.ua.es>
2025-05-22 11:00:49 +0200

    Pequeño cambio

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,9 +15,9 @@ import AssetUpload from './pages/AssetUpload';
 import Profile from './pages/Profile'
 import AssetView from './pages/AssetView'
 import DeleteAccount from './pages/DeleteAccount'
-import MyAssets    from './pages/MyAssets'
-import SearchPage    from './pages/SearchPage'
-import AssetDetail    from './pages/AssetDetail'
+import MyAssets from './pages/MyAssets'
+import SearchPage from './pages/SearchPage'
+import AssetDetail from './pages/AssetDetail'
 import AccessibilityPage from './pages/Accessibility';
 
 import ContactPage from './pages/Contact';
@@ -37,7 +37,12 @@ function App() {
           <Route path='/register' element={<Register />} />
           <Route path="/assetview" element={<AssetView />} />
           <Route path="/search" element={<SearchPage />} />
-          
+          <Route path="/devs" element={<DevsPage />} />
+          <Route path='/asset/:id' element={<AssetView />} />
+          <Route path="/contact" element={<ContactPage />} />
+          <Route path="/terms" element={<TermsPage />} />
+          <Route path="/privacy" element={<PrivacyPage />} />
+
           <Route path='/' element={
             <PrivateRoute>
               <SidebarLayout />
@@ -45,18 +50,10 @@ function App() {
           }>
             <Route path='/my-assets' element={<MyAssets />} />
             <Route path='/assets/:id' element={<AssetDetail />} />
-            <Route path='/asset/:id' element={<AssetView />} />
             <Route path='/upload' element={<AssetUpload />} />
             <Route path='/profile' element={<Profile />} />
             <Route path='/delete-account' element={<DeleteAccount />} />
             <Route path="/accessibility" element={<AccessibilityPage />} />
-
-            <Route path="/contact" element={<ContactPage />} />
-            <Route path="/devs" element={<DevsPage />} />
-            <Route path="/terms" element={<TermsPage />} />
-            <Route path="/privacy" element={<PrivacyPage />} />
-
-
           </Route>
 
           <Route path='*' element={<div>Página no encontrada</div>} />
